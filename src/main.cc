932ccff140579836929da882f349f7347a2ab9e8
gareebprogrammer <gareebprogrammer@gmail.com>
2021-03-20 22:43:56 +0530

    fix shader

--- src/main.cc
+++ src/main.cc
@@ -13,10 +13,9 @@ void framebuffer_size_callback(GLFWwindow* window, i32 width, i32 height)
 } 
 
 float vertices[] = {
-        // positions         // colors
-     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
 };
 
 int main(int argc,const char **argv) {
@@ -27,7 +26,7 @@ int main(int argc,const char **argv) {
     }
     win.load_gl();
     win.set_gl_framebuffer_size_callback(framebuffer_size_callback);
-    
+
     Shader s;
     s.load_shader("shaders/vert.glsl","shaders/frag.glsl");
 
@@ -41,13 +40,13 @@ int main(int argc,const char **argv) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    glBindVertexArray(0); 
+
 
     while(win.is_window_closed()) {
 
