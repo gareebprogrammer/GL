7e2424526eeb15865b92067f8eb650a352932983
gareebprogrammer <gareebprogrammer@gmail.com>
2021-03-23 22:09:12 +0530

    some more shaders

--- src/main.cc
+++ src/main.cc
@@ -44,20 +44,26 @@ float vertices[] = {
         -0.9f, -0.5f, 0.0f,  // left 
         -0.0f, -0.5f, 0.0f,  // right
         -0.45f, 0.5f, 0.0f,  // top  
-    }; 
+}; 
 
 float vertices2 [] = {
-        // second triangle
-        0.0f, -0.5f, 0.0f,  // left
-        0.9f, -0.5f, 0.0f,  // right
-        0.45f, 0.5f, 0.0f   // top
+        // second triangle  // colors
+        0.0f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // left 
+        0.9f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // right
+        0.45f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f  // top 
     };
 
- unsigned int indices[] = {  // note that we start from 0!
+// float vertices2[] = {
+//     // positions         // colors
+//      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+//     -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+//      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+// };
+
+unsigned int indices[] = {  // note that we start from 0!
      0, 1, 3,   // first triangle
      1, 2, 3    // second triangle
- }; 
-
+}; 
 
 int main(int argc,const char **argv) {
 
@@ -97,11 +103,15 @@ int main(int argc,const char **argv) {
     glBindVertexArray(VAO[1]);
     glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // positions //
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // colors
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
     
@@ -118,6 +128,7 @@ int main(int argc,const char **argv) {
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
         s2.apply();
+        s2.set_uniform_1f("xoffset",0.1f);
         glBindVertexArray(VAO[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // buffer swap //
